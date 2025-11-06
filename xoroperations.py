a87d1db8af8a87d9b485d8fb44d59fea75dc58c7
aka 0xth00rf1nn aka c1nn4m0r0ls <136485178+0xm0t0k0@users.noreply.github.com>
2025-11-06 16:22:08 +0200

    fixed bug

--- xoroperations.py
+++ xoroperations.py
@@ -1,16 +1,10 @@
 
-key1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
+from pwn import xor
 
-key2 = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
+k1=bytes.fromhex('a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313')
 
-key2 = bytes(a ^ b for a, b in zip(key1, key2))
+k2_3=bytes.fromhex('c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1')
 
-key3 = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
+flag=bytes.fromhex('04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf')
 
-key3 = bytes( a ^ b for a, b in zip(key2, key3))
-
-flag = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
-
-flag = bytes(a ^ b ^ c ^ d for a, b, c, d in zip (key1, key2, key3, flag))
-
-print(flag.decode())
+print(xor(k1,k2_3,flag))  
